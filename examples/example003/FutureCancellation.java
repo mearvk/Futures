745417da0046a8025599d7e9cbaaf0d6c2efb1df
mearvk <mearvk@outlook.com>
2026-06-24 20:46:35 -0400

    Latest Update...

---
+++ examples/example003/FutureCancellation.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.*;
+
+public class FutureCancellation {
+    public static void main(String[] args) throws Exception {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<String> future = executor.submit(() -> {
+            Thread.sleep(10000);
+            return "Never returned";
+        });
+        Thread.sleep(500);
+        future.cancel(true);
+        System.out.println("Cancelled: " + future.isCancelled());
+        System.out.println("Done: " + future.isDone());
+        executor.shutdown();
+    }
+}
---
+++ examples/example006/CompletableFutureCombine.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.*;
+
+public class CompletableFutureCombine {
+    public static void main(String[] args) throws Exception {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> "Hello");
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> "World");
+        CompletableFuture<String> combined = future1.thenCombine(future2, (a, b) -> a + " " + b);
+        System.out.println(combined.get());
+    }
+}
